062390b988798282166a062cdde57a1cedf647e8
saravana pandi <130064336+saravanapandic@users.noreply.github.com>
2024-07-30 09:37:33 +0000

    initial

--- snowflake_function/snowflake_connection_debug.py
+++ snowflake_function/snowflake_connection_debug.py
@@ -13,7 +13,7 @@ class snowflake_connection:
         self.username=username
         self.password=password
         self.warehouse=warehouse
-        self.role=role
+        self.role=role 
     def connection_debug(self):
         try:
             conn = snowflake.connector.connect(
